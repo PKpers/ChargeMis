9c98ba07a239d9f90cb5d10c45d6398493e8e946
PKpers <kpapad@hexcms.hexfarm.rutgers.edu>
2023-07-11 10:51:10 -0400

    further investigating the charge flips

--- src/MatchedLeptonExplorer.py
+++ src/MatchedLeptonExplorer.py
@@ -81,9 +81,16 @@ float ComputeInvariantMass(Vec_t& pt, Vec_t& eta, Vec_t& phi, Vec_t& index){
 ''')
 
 def set_axes_range(pltObj, Xrange, Yrange):
-    pltObj.GetXaxis().SetRangeUser(Xrange[0], Xrange[1])
-    pltObj.GetYaxis().SetRangeUser(Yrange[0], Yrange[1])
-    return
+    if not Yrange : # Allow the possiblity to modify the range of only one axis
+        pltObj.GetXaxis().SetRangeUser(Xrange[0], Xrange[1])
+        return
+    elif not Xrange :
+        pltObj.GetYaxis().SetRangeUser(Yrange[0], Yrange[1])
+        return
+    else:
+        pltObj.GetXaxis().SetRangeUser(Xrange[0], Xrange[1])
+        pltObj.GetYaxis().SetRangeUser(Yrange[0], Yrange[1])
+        return
 #
 
 def add_Header(title):
@@ -119,48 +126,46 @@ muons = df.Filter("Filter_GetRecoDilepton(LightLepton_Flavor, 2)") \
     .Define("GenLepton_InvMass", "ComputeInvariantMass(GenPart_Pt, GenPart_Eta, GenPart_Phi, LightLepton_GenPartIndex)") \
     .Define("GenLepton_Charge", "MapCharge(GenPart_PDGId, LightLepton_GenPartIndex)") \
     .Define("GenTimesLight_Charge", "LightLepton_Charge*GenLepton_Charge") \
+    .Define("GenTimesLight_ChLeading", "GenTimesLight_Charge[0]") \
+    .Define("GenTimesLight_ChSub", "GenTimesLight_Charge[1]") \
     .Define("LightLepton_ChargeSum", "LightLepton_Charge[0]+LightLepton_Charge[1]") 
 #
 numMuons = muons.Count().GetValue() # Total number of Drell-Yan dimuon events
+
 ## One way of estimating the charge flip ratio 
 # is to compare the charge of a reco mu to the charge of its corresponding gen mu  
 # If there is no charge flip the product charge of gen mu x charge of gen mu = 1  
 # otherwise it will be -1
 # This gives the true number of charge flips
 
-muonsProdFliped = muons.Filter("Any(GenTimesLight_Charge == -1)").Count().GetValue() #  events with charge flips
-muonsProdFlipRatio = muonsProdFliped / numMuons
+muonsProdFliped = muons.Filter("Any(GenTimesLight_Charge == -1)") #  events with charge flips
+muonsProdFliped_count = muonsProdFliped.Count().GetValue()
+muonsProdFlipRatio = muonsProdFliped_count / numMuons
 print("the ture rate of charge flip for muons is: ", muonsProdFlipRatio)
 
 # Plot the charge product histogram
 c = ROOT.TCanvas()
-c.SetLogx(0); c.SetLogy(0)
+c.SetLogx(0); c.SetLogy(1)
 c.SetCanvasSize(800, 800)
 c.SetLeftMargin(0.15)
 c.cd()
 c.SaveAs(outFileMuons+"[")
 
 # Plot the invariant mass of the gen lepton pair at first
-c.SetLogy(1)
 genMuonsMass = muons.Histo1D( ("genMuonsMass", "", 50, 50, 300), "GenLepton_InvMass")
 set_axes_title(genMuonsMass, "M_ll", "counts")
 genMuonsMass.Draw()
 add_Header("gen muons mass")
 c.SaveAs(outFileMuons)
 
-c.SetLogy(0)
-muonsProdLeading_Hist = muons.Define("GenTimesLight_ChLeading", "GenTimesLight_Charge[0]") \
-.Histo1D( ("muonsProdLeading_Hist", "", 50, -2, 2), "GenTimesLight_ChLeading" )
-
+muonsProdLeading_Hist = muons.Histo1D( ("muonsProdLeading_Hist", "", 50, -2, 2), "GenTimesLight_ChLeading" )
 set_axes_title(muonsProdLeading_Hist, "gen charge x reco charge", "counts")
 muonsProdLeading_Hist.SetLineColor(1)
 muonsProdLeading_Hist.Draw()
 
-muonsProdSub_Hist = muons.Define("GenTimesLight_ChSub", "GenTimesLight_Charge[1]") \
-.Histo1D( ("muonsProdSub_Hist", "", 50, -2, 2), "GenTimesLight_ChSub")
+muonsProdSub_Hist = muons.Histo1D( ("muonsProdSub_Hist", "", 50, -2, 2), "GenTimesLight_ChSub")
 muonsProdSub_Hist.SetLineColor(3)
 muonsProdSub_Hist.SetLineStyle(2)
-
 muonsProdSub_Hist.Draw("same")
 
 legend = ROOT.TLegend(0.35, 0.72, 0.45, 0.85)
@@ -173,16 +178,22 @@ legend.Draw("same")
 
 label = ROOT.TLatex()
 label.SetTextSize(0.03)
-label.DrawLatexNDC(0.35, 0.68,"charge flip events: {}".format(muonsProdFliped) )
+label.DrawLatexNDC(0.35, 0.68,"charge flip events: {}".format(muonsProdFliped_count) )
 
 c.SaveAs(outFileMuons)
+
+# Some events apparently have muons with registered charge = 0
+# Save them for further investigation 
+weirdEvents = muons.Filter("Any(GenTimesLight_Charge == 0)")\
+    .Snapshot("tree","/home/kpapad/charge_mis/out/data/DY_1of8_WrdMuons.root")
 
 ## Another way of estimating the charge flip ratio 
 # is to examine the total charge of the ll pair.
 # The DY dimuons are charge neutral and thus in case of a flip, the total charge will be != 0. 
 
-dimuonFlipped = muons.Filter("LightLepton_ChargeSum != 0").Count().GetValue()
-dimuonFlippRatio = dimuonFlipped / numMuons
+dimuonFlipped = muons.Filter("LightLepton_ChargeSum != 0")
+dimuonFlipped_count = dimuonFlipped.Count().GetValue()
+dimuonFlippRatio = dimuonFlipped_count / numMuons
 print("the estimated rate of charge flip for muons is: ",dimuonFlippRatio)
 
 # Plot the total charge histogram
@@ -194,7 +205,7 @@ dimuonCharge_Hist.Draw()
 
 label = ROOT.TLatex()
 label.SetTextSize(0.03)
-label.DrawLatexNDC(0.25, 0.68, "charge flip events: {}".format(dimuonFlipped))
+label.DrawLatexNDC(0.25, 0.68, "charge flip events: {}".format(dimuonFlipped_count))
 
 c.SaveAs(outFileMuons)
 
@@ -205,14 +216,18 @@ electrons = df.Filter("Filter_GetRecoDilepton(LightLepton_Flavor, 1)") \
     .Define("GenLepton_InvMass", "ComputeInvariantMass(GenPart_Pt, GenPart_Eta, GenPart_Phi, LightLepton_GenPartIndex)") \
     .Define("GenLepton_Charge", "MapCharge(GenPart_PDGId, LightLepton_GenPartIndex)") \
     .Define("GenTimesLight_Charge", "LightLepton_Charge*GenLepton_Charge") \
-    .Define("LightLepton_ChargeSum", "LightLepton_Charge[0]+LightLepton_Charge[1]") 
+    .Define("GenTimesLight_ChLeading", "GenTimesLight_Charge[0]") \
+    .Define("GenTimesLight_ChSub", "GenTimesLight_Charge[1]") \
+    .Define("LightLepton_ChargeSum", "LightLepton_Charge[0]+LightLepton_Charge[1]")  \
+    .Define("GenLepton_ChargeSum", "GenLepton_Charge[0] + GenLepton_Charge[1]")
 #
 numElectrons = electrons.Count().GetValue() # Total number of Drell-Yan dielectron events
 
 ## Compare the charge of a reco e to the charge of its corresponding gen e  
 
-electronsProdFliped = electrons.Filter("Any(GenTimesLight_Charge == -1)").Count().GetValue() #  events with charge flips
-electronsProdFlipRatio = electronsProdFliped / numElectrons
+electronsProdFliped = electrons.Filter("Any(GenTimesLight_Charge == -1)") #  events with charge flips
+electronsProdFliped_count = electronsProdFliped.Count().GetValue()
+electronsProdFlipRatio = electronsProdFliped_count / numElectrons
 print("the ture rate of charge flip for electrons is: ", electronsProdFlipRatio)
 
 # Plot the charge product histogram
@@ -231,21 +246,20 @@ genElectronsMass.Draw()
 add_Header("gen electrons mass")
 c.SaveAs(outFileElectrons)
 
-c.SetLogy(0)
-electronsProdLeading_Hist = electrons.Define("GenTimesLight_ChLeading", "GenTimesLight_Charge[0]") \
-.Histo1D( ("electronsProdLeading_Hist", "", 50, -2, 2), "GenTimesLight_ChLeading" )
+#c.SetLogy(0)
+electronsProdLeading_Hist = electrons.Histo1D( ("electronsProdLeading_Hist", "", 50, -2, 2), "GenTimesLight_ChLeading" )
 
 set_axes_title(electronsProdLeading_Hist, "gen charge x reco charge", "counts")
 electronsProdLeading_Hist.SetLineColor(1)
 electronsProdLeading_Hist.Draw()
 
-electronsProdSub_Hist = electrons.Define("GenTimesLight_ChSub", "GenTimesLight_Charge[1]") \
-.Histo1D( ("electronsProdSub_Hist", "", 50, -2, 2), "GenTimesLight_ChSub")
+electronsProdSub_Hist = electrons.Histo1D( ("electronsProdSub_Hist", "", 50, -2, 2), "GenTimesLight_ChSub")
 electronsProdSub_Hist.SetLineColor(3)
 electronsProdSub_Hist.SetLineStyle(2)
 
 electronsProdSub_Hist.Draw("same")
 
+# Plot the legend
 legend = ROOT.TLegend(0.35, 0.72, 0.45, 0.85)
 legend.AddEntry(electronsProdLeading_Hist.GetValue(), "Leading electron", "l")
 legend.AddEntry(electronsProdSub_Hist.GetValue(), "Subleading electron", "l")
@@ -256,27 +270,81 @@ legend.Draw("same")
 
 label = ROOT.TLatex()
 label.SetTextSize(0.03)
-label.DrawLatexNDC(0.35, 0.68, "charge flip events: {}".format(electronsProdFliped))
+label.DrawLatexNDC(0.35, 0.68, "charge flip events: {}".format(electronsProdFliped_count))
+
+c.SaveAs(outFileElectrons)
+
+# Some events apparently have electrons with registered charge = 0
+# Save them for further investigation 
+weirdEvents = electrons.Filter("Any(GenTimesLight_Charge == 0)")\
+    .Snapshot("tree","/home/kpapad/charge_mis/out/data/DY_1of8_WrdElectrons.root")
+
+## Plot separatelly the events with charge flip 
+electronsProdLeadingFliped_Hist = electronsProdFliped.Histo1D( ("electronsProdLeadingFliped_Hist", "", 50, -2, 2), "GenTimesLight_ChLeading" )
+
+set_axes_title(electronsProdLeadingFliped_Hist, "gen charge x reco charge", "counts")
+electronsProdLeadingFliped_Hist.SetLineColor(1)
+electronsProdLeadingFliped_Hist.Draw()
+
+electronsProdSubFliped_Hist = electronsProdFliped.Histo1D( ("electronsProdSubFliped_Hist", "", 50, -2, 2), "GenTimesLight_ChSub")
+electronsProdSubFliped_Hist.SetLineColor(3)
+electronsProdSubFliped_Hist.SetLineStyle(2)
+
+electronsProdSubFliped_Hist.Draw("same")
+
+# Plot the legend  
+legend = ROOT.TLegend(0.4, 0.72, 0.45, 0.85)
+legend.AddEntry(electronsProdLeadingFliped_Hist.GetValue(), "Leading electron", "l")
+legend.AddEntry(electronsProdSubFliped_Hist.GetValue(), "Subleading electron", "l")
+legend.SetFillColor(0)
+legend.SetBorderSize(0)
+legend.SetTextSize(0.03)
+legend.Draw("same")
+
+label = ROOT.TLatex()
+label.SetTextSize(0.03)
+label.DrawLatexNDC(0.35, 0.87, "charge fliped events".format(electronsProdFliped_count))
 
 c.SaveAs(outFileElectrons)
 
 # Examine the total charge of the ll pair.
+recoDielectronFlipped = electrons.Filter("LightLepton_ChargeSum != 0")
+recoDielectronFlipped_count = recoDielectronFlipped.Count().GetValue()
+recoDielectronFlippRatio = recoDielectronFlipped_count / numElectrons
 
-dielectronFlipped = electrons.Filter("LightLepton_ChargeSum != 0").Count().GetValue()
-dielectronFlippRatio = dielectronFlipped / numElectrons
-print("the estimated rate of charge flip for electrons is: ",dielectronFlippRatio)
+genDielectronFlipped = electrons.Filter("GenLepton_ChargeSum != 0")
+genDielectronFlipped_count = genDielectronFlipped.Count().GetValue()
+genDielectronFlippRatio = genDielectronFlipped_count / numElectrons
+
+#print("the estimated rate of charge flip for reco electrons is: ",recoDielectronFlippRatio)
 
 # Plot the total charge histogram
-dielectronCharge_Hist = electrons.Histo1D( ("dielectronCharge_Hist", "", 50, -2, 2), "LightLepton_ChargeSum" )
+recoDielectronCharge_Hist = electrons.Histo1D( ("recoDielectronCharge_Hist", "", 50, -2, 2), "LightLepton_ChargeSum" )
+genDielectronCharge_Hist = electrons.Histo1D( ("genDielectronCharge_Hist", "", 50, -2, 2), "GenLepton_ChargeSum" )
 
-set_axes_title(dielectronCharge_Hist, "Total Charge", "counts")
-dielectronCharge_Hist.SetLineColor(1)
-dielectronCharge_Hist.Draw()
+set_axes_title(recoDielectronCharge_Hist, "Total Charge", "counts")
+recoDielectronCharge_Hist.SetLineColor(1)
+genDielectronCharge_Hist.SetLineColor(3)
+genDielectronCharge_Hist.SetLineStyle(2)
+recoDielectronCharge_Hist.Draw()
+genDielectronCharge_Hist.Draw('same')
 
-label = ROOT.TLatex()
-label.SetTextSize(0.03)
-label.DrawLatexNDC(0.2, 0.68, "charge flip events: {}".format(dielectronFlipped) )
+legend = ROOT.TLegend(0.33, 0.72, 0.45, 0.85)
+legend.AddEntry(recoDielectronCharge_Hist.GetValue(), "reco events", "l")
+legend.AddEntry(genDielectronCharge_Hist.GetValue(), "gen events", "l")
+legend.SetFillColor(0)
+legend.SetBorderSize(0)
+legend.SetTextSize(0.03)
+legend.Draw("same")
 
+c.SaveAs(outFileElectrons)
+
+## Focus on the gen charge flip events
+genDielectronFlipedMass_Hist = genDielectronFlipped.Histo1D( ("genDielectronFlipedMass_Hist", "", 50, -2, 300), "GenLepton_InvMass" )
+set_axes_title(genDielectronFlipedMass_Hist , "M_ll", "counts")
+#set_axes_range(genDielectronFlipedMass_Hist,[-2, 500], [])
+genDielectronFlipedMass_Hist .Draw()
+add_Header("Charge flip gen events")
 c.SaveAs(outFileElectrons)
 
 c.SaveAs(outFileElectrons+"]")
